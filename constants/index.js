423ead91e0d90d0eb9249ae00836cc1ba8288ba4
Utkarsh Singh <utkarsdevendrasingh@gmail.com>
2025-09-06 17:34:07 +0530

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,38 @@
+// Logos
+export const logo = "https://res.cloudinary.com/dff30apwl/image/upload/w_300,f_auto,q_auto/v1757098033/CB_Logo_Transparent_rgv2df.png";
+
+export const logoTwo = "https://res.cloudinary.com/dff30apwl/image/upload/w_300,f_auto,q_auto/v1757097853/ASERR_rcppln.png";
+
+export const logoName = "https://res.cloudinary.com/dff30apwl/image/upload/w_300,f_auto,q_auto/v1757098718/CrushBurg_Name_ai_1_mrjnq4.png";
+
+// Menus
+export const menuOne = "https://res.cloudinary.com/dff30apwl/image/upload/w_1200,f_auto,q_auto/v1757098544/WhatsApp_Image_2025-09-05_at_14.07.08_25a19a79_y5cwaa.jpg";
+
+export const menuTwo = "https://res.cloudinary.com/dff30apwl/image/upload/w_1200,f_auto,q_auto/v1757098513/WhatsApp_Image_2025-09-05_at_14.07.08_6d0d25fe_jgb9yh.jpg";
+
+// Burgers
+export const kingFusionBurger = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757097560/KING_FUSION_BURGER_kdnuch.jpg";
+
+export const crunchyVegBurger = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757097405/Crunchy_veg_dqmkwk.jpg";
+
+export const crunchyTandooriBurger = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757097380/CRUNCHY_TANDOORI_BURGER_jbyomn.jpg";
+
+export const makhniBurger = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757096535/cb_makhani_burger_elaqyx.jpg";
+
+export const liteBurger = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757096483/Cb_lite_burger_kciwu7.jpg";
+
+export const delightBurger = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757096367/Cb_delight_burger_v5hdsf.jpg";
+
+export const crunchyCheeseBurger = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757097369/Cruchy_cheese_burger_ibx7xc.jpg";
+
+// Wraps
+export const contWrap = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757097280/Corn_wrap_p8gufk.jpg";
+
+export const paneerWrap = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757097263/Paneer_wrap_cy66sc.jpg";
+
+// Sandwiches
+export const vegWrap = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757097252/Veg_wrap_iurcuf.jpg";
+
+export const royalSandwich = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757097183/Royal_sandwich_si4eni.jpg";
+
+export const cornAndCheeseSandwich = "https://res.cloudinary.com/dff30apwl/image/upload/w_400,f_auto,q_auto/v1757096588/Corn_and_cheese_sandwich_pimqpz.jpg";
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom"
+import AboutUs from "./pages/AboutUs.jsx"
+import HomePage from "./pages/HomePage.jsx"
+import Menu from "./pages/Menu.jsx"
+import Contacts from "./pages/Contacts.jsx"
+import Store from "./pages/Store.jsx"
+import Navbar from "./components/Navbar.jsx"
+import Footer from "./components/Footer.jsx"
+
+function App() {
+  return(
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/store" element={<Store/>}/>
+        <Route path="/menu" element={<Menu/>}/>
+        <Route path="/aboutus" element={<AboutUs/>}/>
+        <Route path="/contacts" element={<Contacts/>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
